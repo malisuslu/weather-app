979b28520581f4a4c666617a4ed465b84036f365
Muhammed Ali SUSLU <malisuslu@yandex.com>
2022-06-20 05:51:57 +0300

    Updates

---
+++ precache-manifest.3ccb205d18fab93cdc83d8c6a8c12bde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71bc46e270c559c95c9a",
+    "url": "/weather-app/static/js/main.71bc46e2.chunk.js"
+  },
+  {
+    "revision": "e948b0703821c5dbe033",
+    "url": "/weather-app/static/css/1.00b483a5.chunk.css"
+  },
+  {
+    "revision": "e948b0703821c5dbe033",
+    "url": "/weather-app/static/js/1.e948b070.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "2f9ffedc9270eadc9a7baf9e03bf80af",
+    "url": "/weather-app/index.html"
+  }
+];
